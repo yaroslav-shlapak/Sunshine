1048d5585171d3edb2e849961700b442c409e179
y.shlapak <y.shlapak@samsung.com>
2015-04-07 19:17:37 +0300

    Lesson 2 - Refresh button inflater
    Signed-off-by: y.shlapak <y.shlapak@samsung.com>

--- app/src/main/java/com/example/yshlapak/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/yshlapak/sunshine/ForecastFragment.java
@@ -32,7 +32,7 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);m ,. /
 
 
         String[] data = {
